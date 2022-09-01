99f02d1d0613a2cd72989db89055b26c92d58b10
Kevin Van Le <lekevinvan@gmail.com>
2022-09-02 00:35:31 +0200

    updated comment and docstring filter func (from CodeBERT github)

--- scripts/Tokenize_Datasets/interleave_datasets.py
+++ scripts/Tokenize_Datasets/interleave_datasets.py
@@ -1,28 +1,73 @@
 from datasets import load_dataset, interleave_datasets, DatasetDict
 import re
 import argparse
-import pyparsing
+from io import StringIO
+import tokenize
+
+
+def remove_comments_and_docstrings(source,lang):
+    if lang in ['python']:
+        """
+        Returns 'source' minus comments and docstrings.
+        """
+        io_obj = StringIO(source)
+        out = ""
+        prev_toktype = tokenize.INDENT
+        last_lineno = -1
+        last_col = 0
+        for tok in tokenize.generate_tokens(io_obj.readline):
+            token_type = tok[0]
+            token_string = tok[1]
+            start_line, start_col = tok[2]
+            end_line, end_col = tok[3]
+            ltext = tok[4]
+            if start_line > last_lineno:
+                last_col = 0
+            if start_col > last_col:
+                out += (" " * (start_col - last_col))
+            # Remove comments:
+            if token_type == tokenize.COMMENT:
+                pass
+            # This series of conditionals removes docstrings:
+            elif token_type == tokenize.STRING:
+                if prev_toktype != tokenize.INDENT:
+            # This is likely a docstring; double-check we're not inside an operator:
+                    if prev_toktype != tokenize.NEWLINE:
+                        if start_col > 0:
+                            out += token_string
+            else:
+                out += token_string
+            prev_toktype = token_type
+            last_col = end_col
+            last_lineno = end_line
+        temp=[]
+        for x in out.split('\n'):
+            if x.strip()!="":
+                temp.append(x)
+        return '\n'.join(temp)
+    elif lang in ['ruby']:
+        return source
+    else:
+        def replacer(match):
+            s = match.group(0)
+            if s.startswith('/'):
+                return " " # note: a space and not an empty string
+            else:
+                return s
+        pattern = re.compile(
+            r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+            re.DOTALL | re.MULTILINE
+        )
+        temp=[]
+        for x in re.sub(pattern, replacer, source).split('\n'):
+            if x.strip()!="":
+                temp.append(x)
+        return '\n'.join(temp)
 
 
 def map_doc_and_func(example):
-    filtered_func = example["whole_func_string"]
-    if example["language"] == "java":
-        filtered_func = pyparsing.javaStyleComment.suppress().transformString(example["whole_func_string"])
-    elif example["language"] == "go":
-        filtered_func = pyparsing.javaStyleComment.suppress().transformString(example["whole_func_string"])
-    elif example["language"] == "python":
-        filtered_func = pyparsing.pythonStyleComment.suppress().transformString(example["whole_func_string"])
-        filtered_func = pyparsing.nestedExpr('"""', '""').suppress().transformString(filtered_func)
-        filtered_func = pyparsing.nestedExpr("'''", "''").suppress().transformString(filtered_func)
-    elif example["language"] == "ruby":
-        filtered_func = pyparsing.pythonStyleComment.suppress().transformString(example["whole_func_string"])
-        filtered_func = pyparsing.nestedExpr("=begin", "=end").suppress().transformString(filtered_func)
-    elif example["language"] == "php":
-        filtered_func = pyparsing.javaStyleComment.suppress().transformString(example["whole_func_string"])
-    elif example["language"] == "javascript":
-        filtered_func = pyparsing.javaStyleComment.suppress().transformString(example["whole_func_string"])
-    example["text"] = example["func_documentation_string"] + "<c>" + filtered_func
-    return example
+    filtered_func = remove_comments_and_docstrings(example["whole_func_string"], example["language"])
+    return example["func_documentation_string"] + "<c>" + filtered_func
 
 
 def main():
@@ -64,7 +109,7 @@ def main():
             for split in dataset_list[i]:
                 text_column = ["Placeholder"] * len(dataset_list[i][split])
                 dataset_list[i][split] = dataset_list[i][split].add_column("text", text_column)
-                dataset_list[i][split] = dataset_list[i][split].map(map_doc_and_func)
+                dataset_list[i][split] = dataset_list[i][split].map(map_doc_and_func, load_from_cache_file=False)
                 dataset_list[i][split] = dataset_list[i][split].remove_columns([col for col in dataset_list[i][split].column_names if col != "text"])
 
     interleaved_train_dataset = interleave_datasets([x["train"] for x in dataset_list], probabilities=distribution)

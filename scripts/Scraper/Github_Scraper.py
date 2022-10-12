b213fd26a78b2cbfcc69f26c1fd4c446096dfcc0
Kevin Van Le <lekevinvan@gmail.com>
2022-10-12 21:20:29 +0200

    update method

--- scripts/Scraper/Github_Scraper.py
+++ scripts/Scraper/Github_Scraper.py
@@ -43,7 +43,7 @@ def main():
     argparser.add_argument("--lang", type=str, required=True)
     argparser.add_argument("--queries", default="", type=str, required=True)
     argparser.add_argument("--query_size", type=int, default=3)
-    argparser.add_argument("--starcount", type=int, default=5)
+    argparser.add_argument("--starcount", type=int, default=2)
     argparser.add_argument("--output", default="")
     argparser.add_argument("--spokenlanguage", default="de")
     argparser.add_argument("--auth_token", type=str, required=True)
@@ -60,85 +60,86 @@ def main():
 
     # get all possible repos from queries
     for query in tqdm(queries, desc="Queries processed"):
-        status = True
-        while status:
+        while True:
             url = f"https://api.github.com/search/code?q={query}+in:file +language:{args.lang}&per_page=100&page=" \
                   f"{page_index}"
             response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                for repository in response.json()["items"]:
-                    repo_names.add(repository["repository"]["full_name"])
-                time.sleep(75)
-                page_index += 1
-            else:
-                status = False
+            while response.status_code == 403:
+                time.sleep(60)
+                response = requests.get(url, headers=headers)
+            if response.status_code != 200:
+                break
+            for repository in response.json()["items"]:
+                repo_names.add(repository["repository"]["full_name"])
+            time.sleep(5)
+            page_index += 1
 
     # filter repos by starcount and by spoken language
     remove_set = set()
     for repo in tqdm(repo_names, desc="Filterprogress of repos"):
-        status = True
         german_test = 0
         noDocCounter = 0
-        while status:
-            url = f"https://api.github.com/repos/{repo}"
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                if response.json()["stargazers_count"] < args.starcount:
-                    remove_set.add(repo)
-                    status = False
-                else:
-                    # check if repo is spoken language
-                    repo_request = requests.get(
-                        f"https://api.github.com/repos/{repo}/git/trees/master?recursive=1", headers=headers)
-                    repo_json = repo_request.json() if repo_request.status_code == 200 else print(
-                        repo_request.status_code)
-                    if repo_request.status_code is not 200:
-                        status = False
-                        continue
-                    java_files = [file for file in repo_json["tree"] if file["path"].endswith(".java")]
-                    for file in java_files:
-                        if noDocCounter > 50 or not status:
-                            status = False
+        url = f"https://api.github.com/repos/{repo}"
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            print(f"{response.status_code}:{repo} broken.. skip this")
+            continue
+        if response.json()["stargazers_count"] < 2:
+            remove_set.add(repo)
+        else:
+            print(f"{repo} has potential")
+            # check if repo is spoken language
+            repo_request = requests.get(f"https://api.github.com/repos/{repo}/git/trees/master?recursive=1",
+                                        headers=headers)
+            while repo_request.status_code == 403:
+                print("403 error.. wait a little bit..")
+                time.sleep(90)
+                repo_request = requests.get(f"https://api.github.com/repos/{repo}/git/trees/master?recursive=1",
+                                            headers=headers)
+            if repo_request.status_code == 404:
+                repo_request = requests.get(f"https://api.github.com/repos/{repo}/git/trees/main?recursive=1",
+                                            headers=headers)
+            repo_json = repo_request.json() if repo_request.status_code == 200 else print(repo_request.status_code)
+            if repo_request.status_code != 200:
+                continue
+            java_files = [file for file in repo_json["tree"] if file["path"].endswith(".java")]
+            for file in (file for file in java_files if noDocCounter <= 500):
+                try:
+                    result = urlfetch.fetch(file["url"], headers=headers)
+                    data = json.loads(result.content) if result.status_code == 200 else print(
+                        result.status_code)
+                    decoded_content = base64.b64decode(data["content"])
+                    tree = langparser.parse(bytes(decoded_content))
+                    method_captures = method_query.captures(tree.root_node)
+                    for capture in (capture for capture in method_captures if (german_test <= 4 and german_test >= -4)):
+                        if capture[0].prev_sibling.type != "doc_comment" or capture[0].next_sibling.type != \
+                                "doc_comment":
+                            noDocCounter += 1
+                            continue
+                        docstring = capture[0].prev_sibling.text.decode("ISO-8859-1") if \
+                            capture[0].prev_sibling.type == "doc_comment" else \
+                            capture[0].next_sibling.text.decode("ISO-8859-1")
+                        if langdetect.detect(docstring) != args.spokenlanguage:
+                            german_test -= 1
+                            print(german_test, end=" ")
+                            if german_test == -5:
+                                print(f"{repo} removed, because not {args.spokenlanguage}")
+                                remove_set.add(repo)
+                                print("check next repo")
+                                break
+                        else:
+                            german_test += 1
+                            print(german_test, end=" ")
+                            noDocCounter = 0
+                        if german_test == 5:
+                            print(f"{repo} added")
+                            print("check next repo")
                             break
-                        time.sleep(0.1)
-                        try:
-                            result = urlfetch.fetch(file["url"], headers=headers)
-                            data = json.loads(result.content) if result.status_code == 200 else print(
-                                result.status_code)
-                            decoded_content = base64.b64decode(data["content"])
-                            tree = langparser.parse(bytes(decoded_content))
-                            method_captures = method_query.captures(tree.root_node)
-                            for capture in method_captures:
-                                if not status:
-                                    break
-                                if capture[0].prev_sibling.type == "doc_comment" or capture[0].next_sibling.type == \
-                                        "doc_comment":
-                                    docstring = capture[0].prev_sibling.text.decode("ISO-8859-1") if \
-                                        capture[0].prev_sibling.type == "doc_comment" else \
-                                        capture[0].next_sibling.text.decode("ISO-8859-1")
-                                    if langdetect.detect(docstring) != args.spokenlanguage:
-                                        german_test -= 1
-                                        print(german_test, end=" ")
-                                        if german_test < -5:
-                                            print(f"{repo} removed, because not {args.spokenlanguage}")
-                                            remove_set.add(repo)
-                                            status = False
-                                        break
-                                    german_test += 1
-                                    print(german_test, end=" ")
-                                    noDocCounter = 0
-                                    time.sleep(0.1)
-                                    if german_test > 5:
-                                        status = False
-                                else:
-                                    noDocCounter += 1
-                                    if noDocCounter == 50:
-                                        print("no docs found.. skip repo..")
-                                    continue
-                        except:
-                            pass
-            else:
-                status = False
+
+                except:
+                    pass
+                if german_test >= 5 or german_test <= -5 or noDocCounter >= 500:
+                    break
     for repo in remove_set:
         repo_names.remove(repo)
     json_set = jsonpickle.encode(repo_names)
